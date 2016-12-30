63f2f1e805e98fd9fdb22162bcf3c561d28a34de
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-29 17:50:58 -0800

    more number functions

--- src/stdlib.js
+++ src/stdlib.js
@@ -5,8 +5,33 @@
 "use strict";
 var stdlib;
 (function (stdlib) {
+    //returns a number addition or concatenation statement of two variables.
     stdlib.add2 = function (first, second) {
         return first + " + " + second;
+    };
+    //returns a + expression across on array of variables.
+    stdlib.add = function (args) {
+        return args.join(" + ");
+    };
+    //returns a - expression on two variables
+    stdlib.sub2 = function (first, second) {
+        return first + " - " + second;
+    };
+    //returns a * expression on two variables
+    stdlib.mul2 = function (first, second) {
+        return first + " * " + second;
+    };
+    //returns a / expression on two variables
+    stdlib.div2 = function (first, second) {
+        return first + " / " + second;
+    };
+    //returns a % expression on two variables
+    stdlib.rem2 = function (first, second) {
+        return first + " % " + second;
+    };
+    //floor div for two variables.
+    stdlib.fdiv2 = function (first, second) {
+        return "Math.floor(" + first + " / " + second + ")";
     };
 })(stdlib = exports.stdlib || (exports.stdlib = {}));
 //# sourceMappingURL=stdlib.js.map
--- src/stdlib.ts
+++ src/stdlib.ts
@@ -5,7 +5,33 @@
 
 
 export namespace stdlib {
+    //returns a number addition or concatenation statement of two variables.
     export let add2 = (first, second) => {
         return `${first} + ${second}`;
-    }
+    };
+    //returns a + expression across on array of variables.
+    export let add = (args) => {
+        return args.join(" + ");
+    };
+    //returns a - expression on two variables
+    export let sub2 = (first, second) => {
+        return `${first} - ${second}`;
+    };
+    //returns a * expression on two variables
+    export let mul2 = (first, second) => {
+        return `${first} * ${second}`;
+    };
+    //returns a / expression on two variables
+    export let div2 = (first, second) => {
+        return `${first} / ${second}`;
+    };
+    //returns a % expression on two variables
+    export let rem2 = (first, second) => {
+        return `${first} % ${second}`;
+    };
+    //floor div for two variables.
+    export let fdiv2 = (first, second) => {
+        return `Math.floor(${first} / ${second})`;
+    };
+
 }
