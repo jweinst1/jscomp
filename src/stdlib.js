4d380aad6688609fbf1302604a908485f5585818
JoshuaWeinstein <jweinst1@berkeley.edu>
2016-12-29 18:43:27 -0800

    new get funcs

--- src/stdlib.js
+++ src/stdlib.js
@@ -6,11 +6,11 @@
 var stdlib;
 (function (stdlib) {
     //returns two variables split by some custom operator or expression.
-    stdlib.any2 = function (joiner, first, second) {
+    stdlib.any_2 = function (joiner, first, second) {
         return first + " " + joiner + " " + second;
     };
     //returns a number addition or concatenation statement of two variables.
-    stdlib.add2 = function (first, second) {
+    stdlib.add_2 = function (first, second) {
         return first + " + " + second;
     };
     //returns a + expression across on array of variables.
@@ -18,51 +18,51 @@ var stdlib;
         return args.join(" + ");
     };
     //returns a - expression on two variables
-    stdlib.sub2 = function (first, second) {
+    stdlib.sub_2 = function (first, second) {
         return first + " - " + second;
     };
     //returns a * expression on two variables
-    stdlib.mul2 = function (first, second) {
+    stdlib.mul_2 = function (first, second) {
         return first + " * " + second;
     };
     //returns a / expression on two variables
-    stdlib.div2 = function (first, second) {
+    stdlib.div_2 = function (first, second) {
         return first + " / " + second;
     };
     //returns a % expression on two variables
-    stdlib.rem2 = function (first, second) {
+    stdlib.rem_2 = function (first, second) {
         return first + " % " + second;
     };
     //floor div for two variables.
-    stdlib.fdiv2 = function (first, second) {
+    stdlib.fdiv_2 = function (first, second) {
         return "Math.floor(" + first + " / " + second + ")";
     };
     //raises one variable to the power of another.
-    stdlib.exp2 = function (first, second) {
+    stdlib.exp_2 = function (first, second) {
         return "Math.pow(" + first + ", " + second + ")";
     };
     //returns a === expression on two variables
-    stdlib.eq2 = function (first, second) {
+    stdlib.eq_2 = function (first, second) {
         return first + " === " + second;
     };
     //returns a > expression on two variables
-    stdlib.gt2 = function (first, second) {
+    stdlib.gt_2 = function (first, second) {
         return first + " > " + second;
     };
     //returns a < expression on two variables
-    stdlib.lt2 = function (first, second) {
+    stdlib.lt_2 = function (first, second) {
         return first + " < " + second;
     };
     //returns a >= expression on two variables
-    stdlib.ge2 = function (first, second) {
+    stdlib.ge_2 = function (first, second) {
         return first + " >= " + second;
     };
     //returns a <= expression on two variables
-    stdlib.le2 = function (first, second) {
+    stdlib.le_2 = function (first, second) {
         return first + " <= " + second;
     };
     //returns a !== expression on two variables
-    stdlib.ne2 = function (first, second) {
+    stdlib.ne_2 = function (first, second) {
         return first + " !== " + second;
     };
     //negates the current variable
@@ -70,11 +70,11 @@ var stdlib;
         return "!" + elem;
     };
     //returns a || expression over two variables.
-    stdlib.or2 = function (first, second) {
+    stdlib.or_2 = function (first, second) {
         return first + " || " + second;
     };
     //returns a && expression over two variables.
-    stdlib.and2 = function (first, second) {
+    stdlib.and_2 = function (first, second) {
         return first + " && " + second;
     };
     //returns a && expression across on array of variables.
@@ -94,20 +94,36 @@ var stdlib;
         return "var " + name + " = " + value;
     };
     //declares and assigns a new variable to 0.
-    stdlib.newvar0 = function (name) {
+    stdlib.newvar_0 = function (name) {
         return "var " + name + " = " + 0;
     };
     //declares and assigns a new variable to false.
-    stdlib.newvarfalse = function (name) {
+    stdlib.newvar_false = function (name) {
         return "var " + name + " = " + false;
     };
     //declares and assigns a new variable to null.
-    stdlib.newvarnull = function (name) {
+    stdlib.newvar_null = function (name) {
         return "var " + name + " = " + null;
+    };
+    //declares and assigns a new variable to [].
+    stdlib.newvar_arr = function (name) {
+        return "var " + name + " = []";
+    };
+    //declares and assigns a new variable to {}.
+    stdlib.newvar_obj = function (name) {
+        return "var " + name + " = {}";
     };
     //assigns existing field or variable.
     stdlib.assign = function (name, value) {
         return name + " = " + value;
+    };
+    //retrieves a value from an array, string, or object.
+    stdlib.get = function (obj, key) {
+        return obj + "[" + key + "]";
+    };
+    //retrieves a value from an object using dot notation.
+    stdlib.get_obj = function (obj, key) {
+        return obj + "." + key;
     };
 })(stdlib = exports.stdlib || (exports.stdlib = {}));
 //# sourceMappingURL=stdlib.js.map
--- src/stdlib.ts
+++ src/stdlib.ts
@@ -7,11 +7,11 @@
 export namespace stdlib {
 
     //returns two variables split by some custom operator or expression.
-    export let any2 = (joiner, first, second) => {
+    export let any_2 = (joiner, first, second) => {
         return `${first} ${joiner} ${second}`;
     };
     //returns a number addition or concatenation statement of two variables.
-    export let add2 = (first, second) => {
+    export let add_2 = (first, second) => {
         return `${first} + ${second}`;
     };
     //returns a + expression across on array of variables.
@@ -19,57 +19,57 @@ export namespace stdlib {
         return args.join(" + ");
     };
     //returns a - expression on two variables
-    export let sub2 = (first, second) => {
+    export let sub_2 = (first, second) => {
         return `${first} - ${second}`;
     };
     //returns a * expression on two variables
-    export let mul2 = (first, second) => {
+    export let mul_2 = (first, second) => {
         return `${first} * ${second}`;
     };
     //returns a / expression on two variables
-    export let div2 = (first, second) => {
+    export let div_2 = (first, second) => {
         return `${first} / ${second}`;
     };
     //returns a % expression on two variables
-    export let rem2 = (first, second) => {
+    export let rem_2 = (first, second) => {
         return `${first} % ${second}`;
     };
     //floor div for two variables.
-    export let fdiv2 = (first, second) => {
+    export let fdiv_2 = (first, second) => {
         return `Math.floor(${first} / ${second})`;
     };
     //raises one variable to the power of another.
-    export let exp2 = (first, second) => {
+    export let exp_2 = (first, second) => {
         return `Math.pow(${first}, ${second})`;
     };
 
     //returns a === expression on two variables
-    export let eq2 = (first, second) => {
+    export let eq_2 = (first, second) => {
         return `${first} === ${second}`;
     };
 
     //returns a > expression on two variables
-    export let gt2 = (first, second) => {
+    export let gt_2 = (first, second) => {
         return `${first} > ${second}`;
     };
 
     //returns a < expression on two variables
-    export let lt2 = (first, second) => {
+    export let lt_2 = (first, second) => {
         return `${first} < ${second}`;
     };
 
     //returns a >= expression on two variables
-    export let ge2 = (first, second) => {
+    export let ge_2 = (first, second) => {
         return `${first} >= ${second}`;
     };
 
     //returns a <= expression on two variables
-    export let le2 = (first, second) => {
+    export let le_2 = (first, second) => {
         return `${first} <= ${second}`;
     };
 
     //returns a !== expression on two variables
-    export let ne2 = (first, second) => {
+    export let ne_2 = (first, second) => {
         return `${first} !== ${second}`;
     };
 
@@ -78,12 +78,12 @@ export namespace stdlib {
         return `!${elem}`;
     };
     //returns a || expression over two variables.
-    export let or2 = (first, second) => {
+    export let or_2 = (first, second) => {
         return `${first} || ${second}`;
     };
 
     //returns a && expression over two variables.
-    export let and2 = (first, second) => {
+    export let and_2 = (first, second) => {
         return `${first} && ${second}`;
     };
 
@@ -108,22 +108,42 @@ export namespace stdlib {
     };
 
     //declares and assigns a new variable to 0.
-    export let newvar0 = (name) => {
+    export let newvar_0 = (name) => {
         return `var ${name} = ${0}`;
     };
 
     //declares and assigns a new variable to false.
-    export let newvarfalse = (name) => {
+    export let newvar_false = (name) => {
         return `var ${name} = ${false}`;
     };
 
     //declares and assigns a new variable to null.
-    export let newvarnull = (name) => {
+    export let newvar_null = (name) => {
         return `var ${name} = ${null}`;
+    };
+
+    //declares and assigns a new variable to [].
+    export let newvar_arr = (name) => {
+        return `var ${name} = []`;
+    };
+
+    //declares and assigns a new variable to {}.
+    export let newvar_obj = (name) => {
+        return `var ${name} = {}`;
     };
 
     //assigns existing field or variable.
     export let assign = (name, value) => {
         return `${name} = ${value}`;
+    };
+
+    //retrieves a value from an array, string, or object.
+    export let get = (obj, key) => {
+        return `${obj}[${key}]`;
+    };
+
+    //retrieves a value from an object using dot notation.
+    export let get_obj = (obj, key) => {
+        return `${obj}.${key}`;
     };
 }
